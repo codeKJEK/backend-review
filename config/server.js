b815064b0e69c4c1ff072a5ddf9a90aa7857b74a
KJEKcode <kurtiskovel@gmail.com>
2021-11-08 21:21:34 -0500

    added pg connection

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'afd3828032d2d6ab9e11877b8d9675d5'),
     },
   },
 });
